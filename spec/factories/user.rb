4f1acc4d74fcbb015e77654700f1322596708e88
Maxim Kustov <maxim.kustov@nasctech.net>
2013-11-04 13:16:16 +0200

    Mod: user specs

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,11 +1,33 @@
 FactoryGirl.define do
-  factory :admin_user, class: User do
+  factory :user do
+    login 'user'
+    email 'user@example.com'
+    password 'pasSw0rd!'
+    password_confirmation 'pasSw0rd!'
+    full_name 'User'
+    role_id { ::UserRoles.where(name: 'user').first.id }
+    address "4 Pennsylvania Plaza"
+    city "New York"
+    state "NY"
+    zip 10001
+    country "United States"
+  end
+
+  factory :admin, class: User do
     login 'admin'
     email 'admin@example.com'
     password 'pasSw0rd!'
     password_confirmation 'pasSw0rd!'
     full_name 'Administrator'
-    user_role 1
-    addresss "Donetsk"
+    role_id { ::UserRoles.where(name: 'admin').first.id }
+  end
+
+  factory :moderator, class: User do
+    login 'moderator'
+    email 'moderator@example.com'
+    password 'pasSw0rd!'
+    password_confirmation 'pasSw0rd!'
+    full_name 'Moderator'
+    role_id { ::UserRoles.where(name: 'moderator').first.id }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  it "responds to methods" do
-    user = User.first
-    # user.full_name.should == "Admin"
-    # expect(user.full_name).to eq("Admin")
-    expect(user).to respond_to?(:full_name)
+
+  let!(:user) { build :user }
+  let!(:admin) { build :admin }
+  let!(:moderator) { build :moderator }
+
+  it "builds user" do
+    user.full_name.should == "User"
+    user.role_name.should == "user"
+    user.gmaps4rails_address.should == "United States, 4 Pennsylvania Plaza, New York, NY, 10001"
+    # user.to_gmaps4rails.should == "[{\"lat\":40.7509,\"lng\":-73.9943}]"
+  end
+
+  it "builds admin" do
+    admin.full_name.should == "Administrator"
+    admin.role_name.should == "admin"
+  end
+
+  it "builds moderator" do
+    moderator.full_name.should == "Moderator"
+    moderator.role_name.should == "moderator"
   end
 end
