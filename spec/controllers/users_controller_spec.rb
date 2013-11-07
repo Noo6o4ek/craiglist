3f0f25335f1ac09b956e7a4f0490aec3ffcea74b
Maxim Kustov <maxim.kustov@nasctech.net>
2013-11-07 23:38:01 +0200

    Add: users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -38,4 +38,46 @@ describe UsersController do
       response.should be_success
     end
   end
+
+  describe '#destroy' do
+    def do_delete
+      delete :destroy, id: vasya.to_param
+    end
+
+    let!(:comment) { create(:comment, advert_id: advert_by_vasya.id) }
+    let(:vasya) { create(:user) }
+    let(:moder) { create(:moderator) }
+    let!(:advert_by_vasya) { create(:advert, user_id: vasya.id) }
+
+    it 'is success' do
+      do_delete
+      response.status.should eq 302
+    end
+
+    it 'redirects to users path' do
+      do_delete
+      response.should redirect_to("/users")
+    end
+
+    it 'destroys user' do
+      sign_in vasya
+      expect { do_delete }.to change { User.count }.by(-1)
+    end
+
+    it 'destroys advert' do
+      sign_in vasya
+      expect { do_delete }.to change { Advert.count }.by(-1)
+    end
+
+    it 'destroys joined comment' do
+      sign_in vasya
+      expect { do_delete }.to change { Comment.count }.by(-1)
+    end
+
+    it 'renders error' do
+      sign_in moder
+      do_delete
+      flash[:error].should eq "You can't perform this action."
+    end
+  end
 end
