e17b13bf32f0ad6e017a3f679254830e6bcdf08d
Maxim Kustov <maxim.kustov@nasctech.net>
2013-11-10 13:51:03 +0200

    Add: user updates spec

--- spec/requests/user_updates_profile_spec.rb
+++ spec/requests/user_updates_profile_spec.rb
@@ -36,4 +36,47 @@ describe "User updates" do
     click_link "Cancel my account"
     # page.driver.browser.switch_to.alert.accept
   end
+
+  it "profile from users page" do
+    do_login
+    visit users_path
+    click_link("Edit")
+    fill_in "user_zip", with: "12345"
+    click_button "Update"
+    expect(page).to have_content("User was successfully updated.")
+  end
+
+  it "changes values from users page" do
+    do_login
+    visit users_path
+    click_link("Edit")
+    fill_in "user_zip", with: "12345"
+
+    expect{ click_button "Update" }.to change{ User.find(user.id).zip }.from(10001).to(12345)
+  end
+
+  describe "fake updates" do
+    def fake_udate
+      do_login
+      visit users_path
+      click_link("Edit")
+      fill_in "user_zip", with: ""
+      click_button "Update"
+    end
+
+    it "prevetns to save blanks" do
+      fake_udate
+      expect(page).to have_content("can't be blank")
+    end
+
+    it "doesn't change values" do
+      fake_udate
+      expect{ click_button "Update" }.to_not change{ User.find(user.id).zip }
+    end
+
+    it "redirects to edit action" do
+      fake_udate
+      page.should have_content("Edit User")
+    end
+  end
 end
